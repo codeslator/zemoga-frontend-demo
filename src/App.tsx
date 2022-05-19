f21806e6804b698d400390ccd7b1f9628779ffe4
Andres Melendez <codeslator@gmail.com>
2022-05-19 10:49:43 -0400

    Initial commit; Tailwind CSS configuration;

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <p className="text-3xl font-bold underline text-red-500">Hello Vite + React!</p>
+
+    </div>
+  )
+}
+
+export default App
