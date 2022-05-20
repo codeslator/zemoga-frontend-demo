88c210e04245041605d65a3748e1c35a5ac04d48
Andres Melendez <codeslator@gmail.com>
2022-05-20 12:52:47 -0400

    Adding search bar in Navbar; Improving responsive breakpoints in hero image;

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,16 @@ interface HeroProps {
 
 }
 
+//absolute w-[160vw] h-full -top-20 sm:w-[160vw] sm:top-0 2xl:-top-10 lg:h-[104vh] lg:left-0 object-cover mb-3
+//md:-top-10 lg:-top-10 lg:h-[104vh] lg:left-0
+
 const Hero: FC<HeroProps> = ({}) => {
   return (
-    <header className="h-[80vh]">
-      <img src={PopePic} alt="pope" className="absolute w-full -top-10 object-cover mb-3" />
+    <header className="relative h-[80vw] min-h-[35rem] max-h-[38rem] overflow-hidden lg:min-h-screen lg:max-h-screen">
+      <img
+        src={PopePic}
+        alt="pope"
+        className="absolute object-cover w-[160vw] h-full -top-20 sm:object-center md:w-[110vw] md:object-top lg:h-[106vh] lg:-top-10 lg:object-top mb-3" />
     </header>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,17 +7,18 @@ interface NavbarProps {
 const Navbar: FC<NavbarProps> = ({ title }) => {
   return (
     <nav id="nav" className="bg-gradient-to-b from-neutral-900 to-transparent border-gray-200 lg:px-4 dark:bg-gray-800 lg:min-h-[22vh] flex items-center fixed z-[2] w-full">
-      <div className="lg:container w-full lg:w-[80%] lg:px-14 lg:mx-auto flex flex-wrap justify-between items-center">
+      <div className="xl:container w-full xl:w-[80%] pt-4 md:pt-4 lg:pt-0  lg:px-8 xl:px-14 lg:mx-auto flex flex-wrap justify-between items-center">
         <a href="https://flowbite.com" className="flex items-center py-2">
-          <h1 className="self-center text-4xl whitespace-nowrap text-white pl-2">{title}</h1>
+          <h1 className="self-center text-3xl lg:text-4xl whitespace-nowrap text-white md:pl-2">{title}</h1>
         </a>
-        <div className="flex items-center lg:order-2 pr-2">
-          <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 lg:hidden hover:bg-transparent focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700" aria-controls="mobile-menu-2" aria-expanded="false">
-            <svg className="w-10 h-10" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            <svg className="hidden w-10 h-10" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+        <div className="flex items-center lg:order-2 px-2">
+          <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center text-sm text-gray-500 lg:hidden hover:bg-transparent focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700" aria-controls="mobile-menu-2" aria-expanded="false">
+            <svg className="w-9 h-9" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <svg className="hidden w-9 h-9" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            <span className="sr-only">Open main menu</span>
           </button>
         </div>
-        <div className="hidden justify-between items-center w-full h-full lg:flex lg:w-auto lg:order-2 p-20 lg:p-0" id="mobile-menu-2">
+        <div className="hidden justify-between items-center w-full h-full lg:flex lg:w-auto lg:order-2 py-20 lg:p-0" id="mobile-menu-2">
           <div className="relative lg:hidden my-5 lg:my-0">
             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
               <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
@@ -35,9 +36,10 @@ const Navbar: FC<NavbarProps> = ({ title }) => {
               <a href="#" className="block text-xl py-2 my-5 lg:my-0 text-white bg-transparent text-center dark:text-white" aria-current="page">Login / Sign Up</a>
             </li>
             <li>
-              <div className="hidden lg:block">
+              <button type="button" className="hidden lg:block">
+                <span className="sr-only">Open search</span>
                 <svg className="w-10 h-10 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-              </div>
+              </button>
             </li>
           </ul>
         </div>
